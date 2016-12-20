5fc20c6d675f579948a539660887f12c74163189
Andrew Gemmel <andrew.gemmel@live.ca>
2016-12-19 21:42:19 -0500

    Allow all CORS because that's a good idea

--- api.py
+++ api.py
@@ -10,6 +10,8 @@ app = web.application(urls, globals()).wsgifunc()
 
 class proxy:
 	def GET(self, url):
+		web.header('Access-Control-Allow-Origin',      '*')
+		web.header('Access-Control-Allow-Credentials', 'true')
 		if not url:
 			return 'Usage: /proxy/www.google.com to return www.google.com'
 		else:
